6c19ac2b2e9e0ac231899f2894588cca8ad908d6
Max Big <maxbig@MacBook-Air-Max.local>
2025-11-25 14:18:48 +0300

    some commit

--- algorithms/Baseline.cpp
+++
@@ -1,8 +0,0 @@
-#include "Greedy.h"
-
-namespace scheduling_problem::algorithms {
-
-// Глобальный baseline-алгоритм, на который ссылаются RandomSearch / CSAO / ACO
-Greedy BASELINE("BASELINE");
-
-} // namespace
