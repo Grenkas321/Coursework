3ffdc8df758d1344f61129b9af50d38291d2ebbd
Max Big <maxbig@MacBook-Air-Max.local>
2026-04-10 13:31:11 +0300

    committing new visualisation program - with arrows, linking colors to work numbers, adding markers for root vertexes and short white separator between works; also added diagram pictures and interpretation for them

--- visualisation/gantt_and_memory.py
+++ visualisation/gantt_and_memory.py
@@ -1,7 +1,238 @@
 import matplotlib.pyplot as plt
 import random
 import matplotlib.patches as patches
+from matplotlib.patches import FancyArrowPatch, Circle, Ellipse
+from matplotlib.patches import ConnectionPatch
 from collections import defaultdict
+from matplotlib.patches import Polygon
+
+
+
+def draw_horiz_arrow(ax, from_vertex, to_vertex, schedule, color='blue', style='->'):
+    """Прямая линия"""
+    from_coords = get_task_coordinates(schedule, from_vertex)
+    to_coords = get_task_coordinates(schedule, to_vertex)
+    
+    arrow = FancyArrowPatch(
+        (max(from_coords['x_center'], from_coords['x_finish'] - 1), from_coords['y_center'] + 0.1),
+        (min(to_coords['x_center'], to_coords['x_start'] + 1), to_coords['y_center'] + 0.1),
+        arrowstyle=style,
+        color=color,
+        linewidth=1.3,
+        mutation_scale=11,
+        connectionstyle="arc3,rad=0"
+    )
+    ax.add_patch(arrow)
+
+
+def draw_loop_arrow_ellipse(ax, vertex, schedule, color='purple', style='->'):
+    """Эллиптическая петля на одной задаче"""
+    coords = get_task_coordinates(schedule, vertex)
+    
+    # Петля выходит из правого конца и возвращается в левый
+    x_start = coords['x_finish']
+    x_end = coords['x_start']
+    y = coords['y_center']
+    
+    # Используем Angle3 для создания дуги с заданным углом
+    arrow = FancyArrowPatch(
+        (x_start, y),
+        (x_end, y),
+        arrowstyle=style,
+        color=color,
+        linewidth=2,
+        connectionstyle="angle3,angleA=90,angleB=90"  # вертикальные углы
+    )
+    ax.add_patch(arrow)
+
+
+def get_task_coordinates(schedule, vertex, processor=None):
+    """Получить координаты задачи на Gantt-диаграмме"""
+    params = schedule[vertex]
+    proc = processor if processor else params['processor']
+    start = params['start']
+    finish = params['finish']
+    duration = finish - start
+    
+    # Y-координата: процессоры идут сверху вниз, но с инвертированной осью
+    y_center = 0.75 + proc  # Центр полосы
+    y_top = y_center + 0.25  # Верх полосы
+    y_bottom = y_center - 0.25  # Низ полосы
+    
+    return {
+        'start': start,
+        'finish': finish,
+        'duration': duration,
+        'x_center': start + duration/2,
+        'x_start': start,
+        'x_finish': finish,
+        'y_center': y_center,
+        'y_top': y_top,
+        'y_bottom': y_bottom
+    }
+
+
+def draw_vertical_arrow(ax, from_vertex, to_vertex, schedule, color='red', style='->'):
+    """Вертикальная стрелка между задачами"""
+    from_coords = get_task_coordinates(schedule, from_vertex)
+    to_coords = get_task_coordinates(schedule, to_vertex)
+    
+    y1, y2 = (from_coords['y_bottom'] + 0.02, to_coords['y_top'] - 0.04) if from_coords['y_center'] > to_coords['y_center'] else (from_coords['y_top'] - 0.03, to_coords['y_bottom'] + 0.03)
+    
+    arrow = FancyArrowPatch(
+        (from_coords['x_finish'], y1),
+        (to_coords['x_start'], y2),
+        arrowstyle=style,
+        color=color,
+        linewidth=1.3,
+        mutation_scale=11,
+        connectionstyle="arc3,rad=0"  # rad=0 для прямой
+    )
+    ax.add_patch(arrow)
+
+
+def draw_diagonal_arrow(ax, from_vertex, to_vertex, schedule, color='blue', style='->'):
+    """Диагональная стрелка (прямая линия)"""
+    from_coords = get_task_coordinates(schedule, from_vertex)
+    to_coords = get_task_coordinates(schedule, to_vertex)
+    
+    y1, y2 = (from_coords['y_bottom'] + 0.02, to_coords['y_top'] - 0.04) if from_coords['y_center'] > to_coords['y_center'] else (from_coords['y_top'] - 0.03, to_coords['y_bottom'] + 0.03)
+    
+    arrow = FancyArrowPatch(
+        (from_coords['x_finish'], y1),
+        (to_coords['x_start'] + 0.1, y2),
+        arrowstyle=style,
+        color=color,
+        linewidth=1.3,
+        mutation_scale=11,
+        connectionstyle="arc3,rad=0"
+    )
+    ax.add_patch(arrow)
+
+
+def draw_arc_arrow(ax, from_vertex, to_vertex, schedule, color='green', style='->', curvature=0.3):
+    """Дугообразная стрелка"""
+    from_coords = get_task_coordinates(schedule, from_vertex)
+    to_coords = get_task_coordinates(schedule, to_vertex)
+    
+    length = to_coords['x_start'] - from_coords['x_finish']
+    
+    arrow = FancyArrowPatch(
+        (from_coords['x_finish'] - 0.1, from_coords['y_bottom']),
+        (to_coords['x_start'] + 0.2, to_coords['y_bottom']),
+        arrowstyle=style,
+        color=color,
+        linewidth=1.3,
+        mutation_scale=11,
+        connectionstyle=f"arc3,rad={-curvature * 10 / (length**0.8)}"  # положительное значение = дуга вверх
+    )
+    ax.add_patch(arrow)
+
+
+def draw_vert_arc_arrow(ax, from_vertex, to_vertex, schedule, color='green', style='->', curvature=0.3):
+    """Дугообразная стрелка"""
+    from_coords = get_task_coordinates(schedule, from_vertex)
+    to_coords = get_task_coordinates(schedule, to_vertex)
+    
+    arrow = FancyArrowPatch(
+        (from_coords['x_finish'], from_coords['y_bottom']),
+        (to_coords['x_start'], to_coords['y_bottom']),
+        arrowstyle=style,
+        color=color,
+        linewidth=1.3,
+        mutation_scale=11,
+        connectionstyle=f"arc3,rad={-curvature}"  # положительное значение = дуга вверх
+    )
+    ax.add_patch(arrow)
+
+
+def draw_loop_arrow(ax, from_vertex, to_vertex, schedule, color='purple', style='->'):
+    """Петлеобразная стрелка"""
+    from_coords = get_task_coordinates(schedule, from_vertex)
+    
+    if from_vertex == to_vertex:
+        # Петля на себе
+        # Создаем круговую петлю
+        x = from_coords['x_finish']
+        y = from_coords['y_bottom']
+        
+        # Используем ConnectionPatch с круговым путем
+        '''
+        circle = Circle((x, y - 0.2), 0.2, fill=False, 
+                       edgecolor=color, linewidth=1.3)
+        ax.add_patch(circle)
+        '''
+        ellipse = Ellipse(
+            (x, y - 0.2),  # центр
+            width=0.5,
+            height=0.4,
+            angle=0,
+            fill=False,
+            edgecolor=color,
+            linewidth=1.3
+        )
+        ax.add_patch(ellipse)
+        
+        # Добавляем стрелку на конце
+        arrow = FancyArrowPatch(
+            (x + 0.25, y - 0.07),
+            (x - 0.1, y + 0.05),
+            arrowstyle=style,
+            color=color,
+            linewidth=1.3,
+            mutation_scale=11,
+        )
+        ax.add_patch(arrow)
+    else:
+        # Петля с возвратом
+        to_coords = get_task_coordinates(schedule, to_vertex)
+        mid_x = (from_coords['x_finish'] + to_coords['x_start']) / 2
+        mid_y = min(from_coords['y_center'], to_coords['y_center']) - 0.5
+        
+        # Путь через точку ниже
+        arrow = FancyArrowPatch(
+            (from_coords['x_finish'], from_coords['y_center']),
+            (to_coords['x_start'], to_coords['y_center']),
+            arrowstyle=style,
+            color=color,
+            linewidth=1.3,
+            mutation_scale=11,
+            connectionstyle=f"arc3,rad=-0.5"  # отрицательное значение = дуга вниз
+        )
+        ax.add_patch(arrow)
+
+
+def draw_bezier_arrow(ax, from_vertex, to_vertex, schedule, color='orange', style='->'):
+    """Стрелка по кривой Безье"""
+    from_coords = get_task_coordinates(schedule, from_vertex)
+    to_coords = get_task_coordinates(schedule, to_vertex)
+    
+    from matplotlib.path import Path
+    import matplotlib.patches as patches
+    
+    # Контрольные точки для кривой Безье
+    verts = [
+        (from_coords['x_finish'], from_coords['y_center']),  # начало
+        (from_coords['x_finish'] + 1, from_coords['y_center'] + 0.5),  # контрольная 1
+        (to_coords['x_start'] - 1, to_coords['y_center'] + 0.5),  # контрольная 2
+        (to_coords['x_start'], to_coords['y_center'])  # конец
+    ]
+    
+    codes = [Path.MOVETO, Path.CURVE4, Path.CURVE4, Path.CURVE4]
+    
+    path = Path(verts, codes)
+    patch = patches.PathPatch(path, facecolor='none', edgecolor=color, linewidth=2)
+    ax.add_patch(patch)
+    
+    # Добавляем стрелку в конце (можно использовать FancyArrowPatch)
+    arrow = FancyArrowPatch(
+        verts[2], verts[3],
+        arrowstyle=style,
+        color=color,
+        linewidth=2
+    )
+    ax.add_patch(arrow)
+
 
 class ResourceVisualizer:
     def __init__(self):
@@ -95,10 +326,14 @@ class ResourceVisualizer:
 # Создаем фигуру с двумя подграфиками (2 строки, 1 колонка)
 fig, (ax1, ax2) = plt.subplots(2, 1, figsize=(15, 8))
 
-file_name = 'layered_10_buf_times_P2_M2500'
+file_name = 'layered_14_buf_times'
+p_m = '_P4_M2200'
 algo = 'lp'
+title = 'ЛП, много процессоров и памяти => другое оптимальное расписание'
+diag_name = 'lp_14_high_P_and_M.png'
+save0 = 0
 
-with open(f'/Users/maxbig/Coursework_multiprocessing/Coursework/SCIP/SCIPOptSuite-9.2.1-Linux/inputs/new_no_tr/order/{file_name}_down_left_input.lp') as f:
+with open(f'/Users/maxbig/Coursework_multiprocessing/Coursework/SCIP/SCIPOptSuite-9.2.1-Linux/inputs/new_no_tr/order/{file_name + p_m}_down_left_input.lp') as f:
     lines = f.readlines()
     prev_line = ''
     q = 0
@@ -110,8 +345,9 @@ with open(f'/Users/maxbig/Coursework_multiprocessing/Coursework/SCIP/SCIPOptSuit
             M = int(prev_line.split('<=')[1])
         prev_line = line
 
+
 # ==================== ВЕРХНИЙ ГРАФИК (Gantt) ====================
-with open(f'/Users/maxbig/Coursework_multiprocessing/Coursework/build/Answer/{algo}/schedules/best/{file_name}.json') as f:
+with open(f'/Users/maxbig/Coursework_multiprocessing/Coursework/build/Answer/{algo}/schedules/best/{file_name + p_m}.json') as f:
     text = f.readline()
     dct = eval(text)
     name = list(dct.keys())[0]
@@ -122,6 +358,7 @@ with open(f'/Users/maxbig/Coursework_multiprocessing/Coursework/build/Answer/{al
     schedule = dct['schedule']
     
     tasks = {}
+    tasks_v = {}
     for vertex, params in schedule.items():
         processor = params['processor']
         start = params['start']
@@ -131,13 +368,14 @@ with open(f'/Users/maxbig/Coursework_multiprocessing/Coursework/build/Answer/{al
             tasks[processor] = [(start, duration)]
         else:
             tasks[processor] += [(start, duration)]
+        tasks_v[(processor, start, duration)] = vertex
     tasks = dict(sorted(tasks.items()))
     
     # Рисуем полосы
     Y = 0.5
     H = 0.5
     stage = 0.5
-    all_colors = [(1, 0, 0), (1, 0.5, 0), (1, 1, 0), (0.5, 1, 0), (0, 1, 0), (0, 1, 0.5), (0, 1, 1), (0, 0.5, 1), (0, 0, 1), (0.5, 0, 1), (1, 0, 1), (1, 0, 0.5)]
+    all_colors = [(1, 0, 0), (1, 0.5, 0), (0.9, 0.9, 0.1), (0.5, 1, 0), (0, 1, 0), (0, 1, 0.5), (0, 1, 1), (0, 0.5, 1), (0, 0, 1), (0.5, 0, 1), (1, 0, 1), (1, 0, 0.5)]
     random_colors = []
     nums = [i for i in range(12)]
     random.seed(42)
@@ -147,12 +385,24 @@ with open(f'/Users/maxbig/Coursework_multiprocessing/Coursework/build/Answer/{al
         nums = [j for j in range(12)]
         for j in range(c - 2, c + 3):
             nums.remove((12 + j) % 12)
+    
+    random_colors = [all_colors[0], all_colors[3], all_colors[6], all_colors[10], all_colors[1], all_colors[4], all_colors[7], all_colors[11], all_colors[2], all_colors[9], all_colors[5], all_colors[8]] * 100
+    '''
     i = 0
     
-    for task in list(tasks.values()):
+    for num, task in tasks.items():
         c = random_colors[i:i + len(task)]
         i += len(task)
         ax1.broken_barh(sorted(task), (Y, H), facecolors=c)
+        Y += 1
+    '''
+    
+    for prc, task0 in tasks.items():
+        task = sorted(task0)
+        c = [random_colors[int(tasks_v[(prc, strt, dur)])] for strt, dur in task]
+        ax1.broken_barh(sorted(task), (Y, H), facecolors=c)
+        white_bars = [(tsk[0] - 0.05, 0.1) for tsk in task]
+        ax1.broken_barh(white_bars, (Y, H), facecolors=['white'] * len(white_bars))
         Y += 1
     
     for vertex, params in schedule.items():
@@ -170,19 +420,29 @@ with open(f'/Users/maxbig/Coursework_multiprocessing/Coursework/build/Answer/{al
     # Настройка осей для верхнего графика
     ax1.set_yticks([0.75 + k for k in range(P)])
     ax1.set_yticklabels([f'Процессор {k + 1}' for k in range(P)])
-    ax1.set_ylabel('Процессоры', fontsize=12)
-    # ax1.set_title('Gantt-диаграмма выполнения задач', fontsize=14, fontweight='bold')
+    # ax1.set_ylabel('Процессоры', fontsize=12)
+    ax1.set_title(title, fontsize=14, fontweight='bold')
     # ax1.invert_yaxis()
     ax1.grid(True, axis='x', linestyle='--', alpha=0.6)
     ax1.set_xlim(0, makespan)
     ax1.set_ylim(0, 0.75 + P - 1 + 0.75)
     ax1.invert_yaxis()
+    ax1.text(
+        x=makespan - 0.5,  # позиция по X (в долях от ширины графика, 0.02 = 2% от левого края)
+        y=(0.75 + P - 1 + 0.75) * 1.029,     # позиция по Y (на уровне линии)
+        s=f'{makespan}',  # текст подписи
+        color='black',
+        fontsize=10,
+        fontweight='bold',
+        va='bottom' if M < ax1.get_ylim()[1] * 0.9 else 'top',  # автоматическое выравнивание
+        ha='left'
+    )
 
 
 # ==================== НИЖНИЙ ГРАФИК (Memory Usage) ====================
 viz = ResourceVisualizer()
 
-with open(f'/Users/maxbig/Coursework_multiprocessing/Coursework/SCIP/SCIPOptSuite-9.2.1-Linux/translator_inputs/{file_name}.txt') as f:
+with open(f'/Users/maxbig/Coursework_multiprocessing/Coursework/LP/noP/{file_name}.txt') as f:
     bfrs, times = {}, {}
     nodes = []
     sizes = {}
@@ -214,7 +474,41 @@ with open(f'/Users/maxbig/Coursework_multiprocessing/Coursework/SCIP/SCIPOptSuit
     f.close()
     root_parents = list(set(nodes) - nodes_with_par)
 
-with open(f'/Users/maxbig/Coursework_multiprocessing/Coursework/build/Answer/{algo}/schedules/best/{file_name}.json') as f:
+# print(sizes)
+
+for root_v in root_parents:
+    crds = get_task_coordinates(schedule, root_v)
+    
+    width = min(0.007 * makespan, 1)
+    height = min(0.05 * (0.75 + P - 1 + 0.75), 0.5)
+    
+    pts = [[crds['x_center'] - width / 2, crds['y_bottom'] - height], [crds['x_center'] + width / 2, crds['y_bottom'] - height], [crds['x_center'], crds['y_bottom']]]
+    triangle = Polygon(pts, closed=True, color='black', alpha=0.5)
+    ax1.add_patch(triangle)
+
+
+for v1, v2 in list(sizes.keys()):
+    coords1 = get_task_coordinates(schedule, v1)
+    coords2 = get_task_coordinates(schedule, v2)
+    if coords1['x_finish'] == coords2['x_start']:
+        if coords1['y_center'] == coords2['y_center']:
+            # draw_loop_arrow(ax1, v1, v1, schedule, color='black')  # петля на себе
+            draw_horiz_arrow(ax1, v1, v2, schedule, color='black')
+            # draw_arc_arrow(ax1, v1, v2, schedule, color='black', curvature=15, style='->')
+            # draw_loop_arrow_ellipse(ax1, v1, schedule)
+        else:
+            if abs(coords1['y_center'] - coords2['y_center']) == 1:
+                draw_vertical_arrow(ax1, v1, v2, schedule, color='black')
+            else:
+                draw_vert_arc_arrow(ax1, v1, v2, schedule, color='black', curvature=0.2, style='->')
+
+    elif coords1['y_center'] == coords2['y_center']:
+        draw_arc_arrow(ax1, v1, v2, schedule, color='black', curvature=0.2, style='->')
+    else:
+        draw_diagonal_arrow(ax1, v1, v2, schedule, color='black')
+
+
+with open(f'/Users/maxbig/Coursework_multiprocessing/Coursework/build/Answer/{algo}/schedules/best/{file_name + p_m}.json') as f:
     text = f.readline()
     dct = eval(text)
     name = list(dct.keys())[0]
@@ -266,9 +560,35 @@ for t in range(max_fin + 1):
 
 viz.add_horizontal_line(M, '-', 'red', 1)
 
+# Добавить подпись к красной горизонтальной линии
+ax2.text(
+    x=0.2,  # позиция по X (в долях от ширины графика, 0.02 = 2% от левого края)
+    y=M*1.05,     # позиция по Y (на уровне линии)
+    s=f'{M}',  # текст подписи
+    color='red',
+    fontsize=10,
+    va='bottom' if M < ax2.get_ylim()[1] * 0.9 else 'top',  # автоматическое выравнивание
+    ha='left'
+)
+
+ax2.text(
+    x=makespan - 0.5,  # позиция по X (в долях от ширины графика, 0.02 = 2% от левого края)
+    y=-M*0.03,     # позиция по Y (на уровне линии)
+    s=f'{makespan}',  # текст подписи
+    color='black',
+    fontsize=10,
+    fontweight='bold',
+    va='bottom' if M < ax2.get_ylim()[1] * 0.9 else 'top',  # автоматическое выравнивание
+    ha='left'
+)
+
 # Отрисовываем нижний график на ax2
 viz.visualize(ax2)
 # ax2.set_title('Динамика использования памяти', fontsize=14, fontweight='bold')
 
 plt.tight_layout()
+
+if save0:
+    plt.savefig(diag_name, dpi=300, bbox_inches='tight', facecolor=fig.get_facecolor())
+
 plt.show()
--- visualisation/visual6_time.py
+++ visualisation/visual6_time.py
@@ -190,8 +190,8 @@ with open('/Users/maxbig/ASVK/coursework/big_sp_graphs_new/' + graph_name + '.tx
     lines = f.readlines()
 '''
 
-graph_name = 'sp_26_buf_times_P4_M10000'
-with open('/Users/maxbig/Coursework_multiprocessing/Coursework/LP/P4_M10000/sp_26_buf_times_P4_M10000.txt', "r", encoding="utf-8") as f:
+graph_name = 'layered_14_buf_times'
+with open('/Users/maxbig/Coursework_multiprocessing/Coursework/LP/noP/layered_14_buf_times.txt', "r", encoding="utf-8") as f:
     lines = f.readlines()
 
 lines = lines[1:]
